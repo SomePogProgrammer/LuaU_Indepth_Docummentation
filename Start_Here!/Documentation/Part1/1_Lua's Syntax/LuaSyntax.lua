0100e7ee7b531bf3299727e18b9e9a930a52d995
SomeProgrammer <92398408+SomePogProgrammer@users.noreply.github.com>
2023-08-04 14:38:15 -0400

    Add files via upload

---
+++ Start_Here!/Documentation/Part1/1_Lua's Syntax/LuaSyntax.lua
@@ -0,0 +1,209 @@
+--[[
+    ___									   	   _____
+	|  |			| |		  | |	       	  /  _  \
+	|  |			| |		  | |	     	 /	/ \  \
+	|  |			| |		  | |	    	/  /___\  \
+	|  |			| |		  | |	       /  ______   \
+	|  |   			| |		  | |	   	  /	 /	     \  \
+	|  |			| |		  | |	 	 /	/		  \  \
+	|  |			| |		  | |	 	/  /		   \  \
+	|  |_ _ _ _ _	\  \_ _ _ / / 	   /  /		        \  \
+	|_ _ _ _ _ _ |	 \_ _ _ _ _/	  /__/		         \__\
+	
+	
+]]--
+
+
+
+
+
+------- /// Variables \\\ -------
+local myString = "text" -- A value encased in '', "", or `` is considered text or a "String"
+local myNumber = 1 -- A Integer/Number value, can be any numerical value, it can also be an equation, but it always must return a number
+local boolValue = true -- A Bool (Boolean) is just a true or false value
+local myArray = {"Dog","Cat","Fish"}
+local myTable = {
+	Name = "John",
+	Age = 42,
+	Race = "Murican"
+}
+
+print(myString) -- [[ Strings = Text ]]
+print(myNumber) -- [[ Integers/Numbers = Always returns a single number. But myNumber can be set to an equation like: myNumber = 1+1]]
+print(myArray) --[[  Arrays = Just Values (Auto Indexed)
+{
+ [1] = "Dog",
+ [2] = "Cat",
+ [3] = "Fish",
+} 
+]]
+
+print(myTable) --[[ Tables = Variables + Values (Not Auto Indexed)
+{
+ Name = "John",
+ Age = 42,
+ Race = "Murican"
+}
+]]
+
+------- /// Conditionals / Loops \\\ -------
+
+if boolValue then -- If myTable has a value, if so then it returns true, and runs
+	-- Code here
+end
+
+--- "return" will stop the current function or condition
+
+if myTable["Age"] >= 18 then 
+	print("Tax that man")
+else 
+	print("No taxes yet.")
+end 
+
+
+while true do -- Inf loop, only stops when the script stops running. 
+	-- This runs when the code inbetween while ____ do returns a value that isn't nil/null or false
+	break
+end
+
+--- "break" inside of a if statement, for loop, or while loop will stop it. Return won't work for the while loop
+
+-- The for loop has multiple parts,
+
+--[[
+for (variable = starting value), (end value) do
+	-- code
+end
+]]
+	
+
+-- Example: 
+
+for Num = 0, 10 do --< Runs until Number = 10
+	-- Each time this runs, Num's value is upped by 1, until it reaches 10
+end
+
+for timez,var in ipairs(myTable) do -- Loops through a table, until it gets to the last entry
+	
+end
+
+for timez,var in pairs(myArray) do  -- Loops through an array, until it gets to the last entry
+	
+end
+
+------- /// Functions \\\ -------
+--[[ 
+ 
+
+_________ PARTS OF A FUNCTION _________
+
+   { 
+  	 1: Function  	
+   	 2: Func Name    
+  	 3: Paramaters (Inside the () there can be paramaters, or predefined variables),
+   	 4: end,
+   }
+
+	There is 1 Function type, however you can write it 3 ways
+--]]
+
+-- [1]: a Global "Function" (Can be called from anywhere below it in the script) Ex:
+
+
+function myGlobalFunction(isThisAFunction)  -- Can be empty
+	-- Code here
+	print(isThisAFunction) -- returns true
+	
+end
+
+myGlobalFunction(true) -- This fires the function, can be written anywhere below the function.
+
+
+-- [2]: a Local "Function" (Can be called from anywhere below it, if it's in the same function) Ex:
+
+function holderFunction() 
+
+	local function myLocalFunction()
+		
+	end
+	
+	myLocalFunction()
+end
+-- myLocalFunction CANNOT BE CALLED outside of the function it originates in.
+holderFunction() -- This function is global
+--[[
+
+  [3]: Connect "Function" (It requires a event to be connected to it.)
+
+	Parts of Connect "Function": 
+	{
+	  1: Variable (Must be somesort of an Instance, or Service)
+	  2: Event, a event is modeled by the ⚡ emoji on the drop down menu.
+	  3: :Connect()
+	  4: Function + Params (This function cannot be named)
+	}
+
+--]]
+-- Ex:
+
+--  Variable  | Event  | Connect | Function
+game.Workspace.Changed:Connect(function()  -- Runs whenever the event is triggered
+	-- Code here
+end) --- MAKE SURE for every open ( you add closed ), same with [], {}, "", '', ``
+
+
+--[[ 
+If you want to call the function you must make it a variable,
+however, there isn't a need to do this unless you need to call the function-
+another time. 
+--]]
+
+--[[
+
+_________ Global Variables _________
+
+Global variables, can be accessed between scripts 
+
+ Parts of a global var:
+ 1: _G
+ 2: .
+ 3: Variable name
+ 
+ Global variables are differnt for the server and client,
+ so if I made a global variable in a ServerScript, only the server can access it.
+ If I made it in a LocalScript, only the Client can access it.
+--]]
+
+_G.ServerAge = 100
+
+--[In another Server Script you can do]--
+
+print(_G.ServerAge)
+
+--[[
+
+	_________ MATH! Yay :sob:  _________
+	
+--]]
+
+local min = 1
+local max = 100
+
+math.random(min,max) -- Returns a whole value, between the two numbers.
+huge =math.huge -- returns the max Integer value 
+math.min(205,50,20,205,21,28459,39,1) --- Returns the lowest int value, without it having to be ordered.
+math.max(205,50,20,205,21,28459,39,1) --- Returns the highest int value, without it having to be ordered.
+math.rad(max) --- Returns the angle
+math.round(10.03000301) --- Rounds to nearest whole value
+pi = math.pi --- Returns pie 🥟 (not that, this: π)
+
+
+Vector3.new(0,0,0) -- (X , Y , Z) for a Position or Size or just a vector 3
+Vector2.new(0,0) -- (X,Y) Horizontal and Vertical
+CFrame.new(Vector3.new(0,0,0),Vector3.new(0,0,0)) -- (X , Y , Z)^2, a Cordinate Frame, has a Position (Vector 3) and Orienation (Vector 3)
+---			Position         ,    Orientation
+--[[
+
+_________ End of Lua Syntax _________
+
+--]]
---
+++ Start_Here!/Documentation/Part1/2_Client To Server Communication/HowItWorks.lua
@@ -0,0 +1,228 @@
+--[[
+    ___									   	   _____
+	|  |			| |		  | |	       	  /  _  \
+	|  |			| |		  | |	     	 /	/ \  \
+	|  |			| |		  | |	    	/  /___\  \
+	|  |			| |		  | |	       /  ______   \
+	|  |   			| |		  | |	   	  /	 /	     \  \
+	|  |			| |		  | |	 	 /	/		  \  \
+	|  |			| |		  | |	 	/  /		   \  \
+	|  |_ _ _ _ _	\  \_ _ _ / / 	   /  /		        \  \
+	|_ _ _ _ _ _ |	 \_ _ _ _ _/	  /__/		         \__\
+	
+	CLIENT 🙍 ->  TO  -> SERVER  🖥️   COMMUNICATION 
+]]--
+	
+
+
+
+--[[
+
+⚠️ THIS WILL BE MOSTLY TEXT sorry :( ik boring, but THIS IS VERY IMPORTANT TO UNDERSTAND ⚠️
+
+
+So what is Client -> Server Communication❓
+
+Basically, 
+in order for you to understand this you need to understand there are 2 Parts of Development:
+
+[1]: Front End (Client, Runs on the Player's Computer or Router), 
+and 
+[2]: Back  End (Server, Runs on the Server roblox provides.)
+
+There's perks to each side, they both are needed to make a multiplayer game. 
+
+---------------------- ///// 🙍 CLIENT 🙍 \\\\\ ----------------------
+
+Lets start off with what the CLIENT OFFERS: 
+]]
+THECLIENT = {
+
+	[1] = {
+		`The player is automatically defined, when programming on the client`,
+		`you can get the player via:`, game.Players.LocalPlayer
+	},
+	[2] = {
+		`You don't have to worry about if your affecting other player's performances.`
+	},
+	[3] = {
+		`Your able to use certain Services that the server can't, such as` 
+			, game:GetService("UserInputService"), game:GetService("RunService") 
+	},
+	[4] = {
+		`Your able to program, create, and manipulate the player's User Interface (UI)`
+	}
+		
+}
+
+--[[
+The client changes stuff only for the LocalPlayer, so keep that in mind when using the client,
+If you create a part on the client, only the LocalPlayer will be able to see it. That part also
+won't be visible from the server. Any changes made by the client aren't visible by the server.
+
+
+---------------------- ///// 🖥️ SERVER 🖥️ \\\\\ ----------------------
+
+Lets talk about what the SERVER OFFERS:
+--]]
+
+THESERVER = {
+	[1] = {
+		`The server can communicate with the client and creates stuff for every player in the game.`,
+		`The server can send messages to all connected clients.`
+	},
+	[2] = {
+		`The server is much more secure, as the client can't access certain things on the server.`,
+		`This means you can store data on the server and not have to worry if other clients can access it`,
+		`Watch out for Remotes, if handled improperly they can be spammed and crash your server!`
+	},
+	[3] = {
+		`The server allows you to manipulate the workspace for every client, along with picking out individual players,`,
+		`The server allows you to use APIs and certain services to store data, and unlock the full potential of your game!`,
+		`These services include,` , game:GetService("DataStoreService") , game:GetService("HttpService")
+	},
+	[4] = {
+		`Using the server you can manipulate the server far better, and you can use it to communicate to other clients,`,
+		`You can also use the server to individually manipulate UI, or create GUI (Generated User Interface) and parent it`,
+		`on event, or on click, just make sure you handle it properly as to not affect other clients.`
+	}
+}
+
+--[[
+
+---------------------- ///// ⚠️IMPORTANT TO REMEMBER⚠️  \\\\\ ----------------------
+
+---
+The Client and Server can always view ReplicatedStorage
+
+Only the Server Can view ServerScriptService & ServerStorage
+---
+
+The Client, CAN read changes from the server.
+
+The Server, CANNOT read changes from the client. 
+
+---------------------------------------------
+HOW DO I CHANGE THAT? 
+
+Well lets say you want the server to know something but your scripting in a localscript
+no worries, there is something called a "Remote", there are 2 Remotes, the RemoteEvent and RemoteFunction.
+
+Difference? Simple. 
+
+RemoteEvents: 
+
+When you fire the client or the server using a remote event, you can connect it to a function.
+The script that fired it doesn't expect a response back. 
+
+Example:
+]]
+
+--- SERVER SCRIPT ---
+
+function serverScript1() 
+	local RemoteEvent = Instance.new("RemoteEvent") --- Imagine this remote is created by the server
+	RemoteEvent.Parent = game:GetService("ReplicatedStorage")
+	RemoteEvent.Name = "RemoteEvent"  
+
+	RemoteEvent.OnServerEvent:Connect(function(plr,Team) -- Player is automatically defined 
+		if game.Teams[Team] then -- Makes sure the Team Exists
+			plr.Team = game.Teams[Team]
+		end
+	end)
+end
+
+serverScript1()
+
+--- LOCAL SCRIPT ---
+-- (Lets say this script is a team change UI programmed on the Client)
+-- In order for the player's team to change it must be done by the server, so we need to let the server know
+
+function localscript1()
+	local RemoteEvent = game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvent")
+	local Team  = "Red"
+
+	RemoteEvent:FireServer(Team) 
+end
+localscript1()
+-- When firing from the client, it automatically sends the plr, along with any other params.
+--[[
+
+Your also able to fire an event from the server to a specific client
+here's an example:
+--]]
+
+--- SERVER SCRIPT ---
+game.Players.PlayerAdded:Connect(function(plr)
+	local RemoteEvent = Instance.new("RemoteEvent") --- Imagine this remote is created by the server
+	RemoteEvent.Parent = game:GetService("ReplicatedStorage")
+	RemoteEvent.Name = "RemoteEvent"  
+
+	local data = 10
+	
+	RemoteEvent:FireClient(plr,data) -- If you want to fire to a specific client (player)
+	
+	RemoteEvent:FireAllClients(data) -- If you want to fire to all clients (players)
+end)
+
+--- LOCAL SCRIPT ---
+
+local RemoteEvent = game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvent")
+
+RemoteEvent.OnClientEvent:Connect(function(data) -- Recieves event
+	local plr = game.Players.LocalPlayer
+	local UI = plr.PlayerGui.MoneyUI
+	
+	UI.CoinText.Text = tostring(data)
+end)
+
+
+--[[
+
+------------------ REMOTE FUNCTIONS ------------------
+
+Now we just saw how amazing RemoteEvents are, what's the difference though between
+Remote Events and Remote Functions?
+
+
+Well the difference is that Remote Functions when fired, 
+they return a value back to the script which fired it.
+
+Heres an example (Client -> Server):
+]]
+
+--- SERVER SCRIPT ---
+function serverScript2()
+	local RemoteFunction = Instance.new("RemoteFunction")
+	RemoteFunction.Parent = game:GetService("ReplicatedStorage")
+	RemoteFunction.Name = "RemoteFunction"
+	local SessionData = {}
+	
+	RemoteFunction.OnServerInvoke(function(plr,data)
+	if data then
+		SessionData[plr.Name] = data
+		return "Data added successfully"
+	else
+		return "Error with adding data"
+	end
+end)
+end
+serverScript2()
+
+--- LOCAL SCRIPT ---
+-- (Lets say we had a save data button)
+local plr = game.Players.LocalPlayer
+local RemoteFunction = game:GetService("ReplicatedStorage"):WaitForChild("RemoteFunction")
+local UI = plr.PlayerGui.UI
+local plrData = {} 
+local updateBtn = UI.updateBtn
+
+updateBtn.MouseButton1Click:Connect(function()
+	local dataSaveStatus = RemoteFunction:InvokeServer(plrData) -- Expects it to return a string
+	-- The script will wait until it gets a response from the server (response = returned value)
+	updateBtn.Text = dataSaveStatus
+	task.wait(3)
+	updateBtn.Text = "Click To Save Data"
+end)
+
+--]]
